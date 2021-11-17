58abbf3d3d50297e72b7a63b5f838bc933647e54
Prasanth Dornala <prasanth.dornala@mitratech.com>
2021-11-18 00:03:52 +0530

    initial changes of portfolio

---
+++ build/precache-manifest.cf12643d20f3ea7795149cd02e0165ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95a4f86a73cee2347c69",
+    "url": "/static/css/main.20607c82.chunk.css"
+  },
+  {
+    "revision": "95a4f86a73cee2347c69",
+    "url": "/static/js/main.95a4f86a.chunk.js"
+  },
+  {
+    "revision": "0c206070c840bf2ff39c",
+    "url": "/static/js/1.0c206070.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0f4c4e721a26b77054659575c706336",
+    "url": "/index.html"
+  }
+];
---
+++ src/resumeData.js
@@ -0,0 +1,198 @@
+let resumeData = {
+  imagebaseurl: 'ttps://github.com/PrashanthDornala/',
+  name: 'Prasanth Dornala',
+  role: 'Full Stack Dot Net Developer',
+  linkedinId: 'Your LinkedIn Id',
+  skypeid: 'Your skypeid',
+  roleDescription:
+    ' I like dabbling in various parts of web development and like to learn about new technologies.',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/prasanth-dornala/',
+      className: 'fa fa-linkedin',
+    },
+    {
+      name: 'github',
+      url: 'https://github.com/PrashanthDornala/',
+      className: 'fa fa-github',
+    },
+    {
+      name: 'skype',
+      url: 'https://twitter.com/Dornalaprashan1',
+      className: 'fa fa-twitter',
+    },
+  ],
+  aboutme:
+    'Hello, My name is PRASANTH DORNALA. I am a Full Stack Dot Net Developer from Hyderabad, Telangana, India. I love working on a variety of technologies in Web Development. My goal is to build highly performant applications that solve real-world problems and provide users with an awesome experience.',
+  mobileNo: '+91 6301174367',
+  address: 'Hyderabad, Telangana, India',
+  email: 'prasanth.official111@gmail.com',
+  education: [
+    {
+      UniversityName: 'SWARNA BHARATHI ISTITUTE OF SCIENCE TECHNOLOGY',
+      specialization: 'BATCHELOR OF TECHNOLOGY (MECH)',
+      MonthOfPassing: 'Aug',
+      YearOfPassing: '2017',
+      Achievements: '',
+    },
+    {
+      UniversityName: 'SUSEELA MEMORIAL JUNIOR COLLEGE',
+      specialization: 'INERTMEDIATE EDUCATION (MPC)',
+      MonthOfPassing: 'July',
+      YearOfPassing: '2013',
+      Achievements: '',
+    },
+  ],
+  work: [
+    {
+      CompanyName: 'ABJYAON CONSULTANCY PRIVATE LIMITED',
+      Designation: 'Associate Consultant',
+      DateOfJoining: 'Oct 2020',
+      DateOfLeaving: 'Still Working',
+      MonthOfLeaving: '',
+      YearOfLeaving: '2018',
+      Achievements: '',
+    },
+    {
+      CompanyName: 'PUBHUB TECH INDIA PRIAVTE LIMITED',
+      Designation: 'Software Developer',
+      DateOfJoining: 'Sep 2018',
+      DateOfLeaving: 'Sep 2020',
+      MonthOfLeaving: '',
+      YearOfLeaving: '2018',
+      Achievements: '',
+    },
+  ],
+  frontEndSkillsDescription: 'Front End Tech',
+  frontEndskills: [
+    {
+      skillname: 'HTML5',
+    },
+    {
+      skillname: 'CSS',
+    },
+    {
+      skillname: 'BootStrap',
+    },
+    {
+      skillname: 'JavaScript',
+    },
+    {
+      skillname: 'JQuery',
+    },
+    {
+      skillname: 'Reactjs',
+    },
+    {
+      skillname: 'Vuejs',
+    },
+  ],
+  backEndSkillsDescription: 'Back End Tech',
+  backEndSkills: [
+    {
+      skillname: 'C#',
+    },
+    {
+      skillname: 'ASP.NET',
+    },
+    {
+      skillname: 'ASP.NET MVC',
+    },
+    {
+      skillname: 'ASP.NET CORE',
+    },
+    {
+      skillname: 'WEB API',
+    },
+  ],
+  devToolsDescription: 'Development Tools',
+  devToolsSkills: [
+    {
+      skillname: 'Visual Studio 2019',
+    },
+    {
+      skillname: 'Visual Studio Code',
+    },
+    {
+      skillname: 'Micrpsoft Azure',
+    },
+    {
+      skillname: 'SQL Server 2018',
+    },
+    {
+      skillname: 'Oracle',
+    },
+    {
+      skillname: 'JIRA',
+    },
+    {
+      skillname: 'Bitbucket',
+    },
+    {
+      skillname: 'Git',
+    },
+    {
+      skillname: 'WIX Toolset',
+    },
+  ],
+
+  portfolio: [
+    {
+      name: 'MS SIMULATOR',
+      client: 'Thermo Fisher Scientific',
+      description:
+        'MS simulator is an existing application with in Thermo Fisher IT landscape. The simulator is used to simulate data acquisition from devices. It has been used across Thermo Fisher to test the device driver software without connecting to a real expensive device. The existing simulator is not meeting the future requirement of TFS as it was built on the old technology. As part of this project, the existing simulator will be re-written using modern technologies (C#, .Net Core, Vue.js, API).',
+      imgurl: 'images/portfolio/phone.jpg',
+    },
+    {
+      name: 'J&K POWER DEVELOPMENT DEPAERTMENT',
+      client: 'JKPDD',
+      description:
+        'JKPDD  is a customer service portal provides Meter, Usage, and billing information. The source of truth for ADSS is Oracle application CCB (Customer care Billing) and MDM (Meter Data Management). This Product directly and indirectly provides the service to different Consumers’.',
+      imgurl: 'images/portfolio/project.jpg',
+    },
+    {
+      name: 'project3',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/project2.png',
+    },
+    {
+      name: 'project4',
+      description: 'mobileapp',
+      imgurl: 'images/portfolio/phone.jpg',
+    },
+  ],
+  testimonials: [
+    {
+      description: 'MS SIMULATOR',
+      name:
+        'MS simulator is an existing application with in Thermo Fisher IT landscape. The simulator is used to simulate data acquisition from devices. It has been used across Thermo Fisher to test the device driver software without connecting to a real expensive device. The existing simulator is not meeting the future requirement of TFS as it was built on the old technology. As part of this project, the existing simulator will be re-written using modern technologies (C#, .Net Core, Vue.js, API).',
+      client: 'Thermo Fisher Scientific',
+      techStack: 'C#, .NET CORE, Web API, VueJS, Microsoft Azure.',
+    },
+    {
+      description: 'J&K POWER DEVELOPMENT DEPAERTMENT',
+      name:
+        'JKPDD  is a customer service portal provides Meter, Usage, and billing information. The source of truth for ADSS is Oracle application CCB (Customer care Billing) and MDM (Meter Data Management). This Product directly and indirectly provides the service to different Consumers.',
+      client: 'JKPDD',
+      techStack: 'ReactJS, Node.js, Material-UI, Jira.',
+    },
+    {
+      description: 'HRMS-ATTENDANCE & HR MANAGEMENT SYSTEM',
+      name:
+        'Attendance management is the act of managing attendance or presence in a work setting to minimize loss due to employee downtime. It mainly supposed to give login, logout timings, and salary calculations based on their PHR’s and performance and different types of reports. Attendance management goes beyond this to provide a working environment which maximizes and motivates employee attendance, which also allow for cross-linking between attendance data and performance',
+      client: 'APEX COVANTAGE INDIA PRIVATE LIMITED',
+      techStack: 'Asp.Net MVC, Entity Framework, C#, JavaScript, Jira.',
+    },
+    {
+      description: 'PQA Tracker – Product QA Tracker ',
+      name:
+        'Product QA Tracker is an online quality tracking module where in, facilities will submit the batches and QA team will take the allocation for the submitted batches’ and do the QA. Once the QA is done, the QA results will be updated in the systems and systems will auto generate mails to all the facilities, as well as delivery team about the status. Using this application, QA Manager can check the team efficiency. Even quality group can generate various reports like Batch Status, QAG Certificates, List of errors, etc.',
+      client: 'APEX COVANTAGE INDIA PRIVATE LIMITED',
+      techStack: 'Asp.Net, Entity Framework, C#, Telerik Controls, Jira.',
+    },
+  ],
+}
+
+export default resumeData
